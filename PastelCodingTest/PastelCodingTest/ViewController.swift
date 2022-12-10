421bd6c840aa9af419a22e7325032aaa8a34e137
Bakre waris <bakarewariz012@gmail.com>
2022-12-10 16:42:33 +0100

    Add pod, realmSwift, alamofire to the project

---
+++ PastelCodingTest/PastelCodingTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PastelCodingTest
+//
+//  Created by mac on 10/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
